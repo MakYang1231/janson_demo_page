44c64c4245577174baccfe5cb232a14488ee5ba7
Mak <makyang.test@gmai.com>
2024-04-12 13:18:26 +0800

    Updates

---
+++ static/CZ5gZd9J.js
@@ -0,0 +1,31 @@
+import{_ as b}from"./DoWy_ff4.js";import{j as h,P as S,y as l,J as a,z as t,A as r,u as s,M as n,Q as d,R as v,U,x as c,V as u,W as y,X as j,Y as _,Z as L,L as $,_ as I,$ as P}from"./KAey-YnG.js";import"./DGyU3URc.js";const k={class:"banner"},G={class:"site-main"},x={id:"product",class:"sect1"},C={class:"container"},E={class:"product-set",role:"toolbar"},T=a("div",{class:"products-sect-title"},[a("span",null,"工廠設備"),n(),a("div",{class:"more"},[a("a",{href:"#"},"more")])],-1),V={class:"products-list"},A={class:"txt"},B={class:"name"},F=v(`<section id="about" class="sect5"><div class="container"><div class="service-sect wow animate__fadeInLeft" data-wow-delay="0.3s"><div class="service-sect-title"><span>服務項目</span></div> <div class="editor clearfix">
+                        1、一、二类压力容器制造（编号：TS2244032-2025）
+                        <br>
+                        2、GB2热力管道和GC2工业管道安装（编号：TS3844015-2025）
+                        <br>
+                        3、D 类（一、二类）压力容器设计资格 （编号 TS2244032-2025）
+                        <br>
+                        4、GC2级压力管道设计资格 （编号TS1844005-2025）
+                        <br>
+                        5、食品卫生级配管
+                        <br>
+                        6、生产各类不锈钢、碳钢反应容器、换热容器、分离容器、储存容器、塔、罐、釜、槽等
+                        <br>
+                        7、各类不锈钢食品桶槽（适用于茶类、乳品、果汁、果冻、饮料、酒类、制药、化妆品等）
+                        <br>
+                        及整厂设备的设计、制造、安装
+                        <br>
+                        8、各种类型搅拌机械（适用于糖类、糕点类、酱料类、胶类等）
+                        <br>
+                        9、各类粉体混合机、化学制药食品桶、蒸汽回转锅、西式厨房设备、制水和环保设备等
+                        <br>
+                        10、化工、食品管式及桶式过滤设备
+                        <br></div> <a href="#" class="btn more">
+                        more
+                    </a></div></div></section>`,1),M=v(`<section class="sect2"><div class="container"><div class="about-sect"><div class="txt-box wow animate__fadeInRight" data-wow-delay="0.3s"><div class="about-sect-title">
+                            關於我們
+                        </div> <div class="editor clearfix">
+                            本公司在台湾成立已有三十多年的历史，是专业生产各种化工、食品设备的厂家。于1997年8月在大陆广东省设立分公司。公司占地面积20亩，其中生产区占地7000平方米。
+                            <br>
+                            公司的质量方针：坚持以客户为中心、以质量求生存、以管理求效益、以效益求发展。我們坚持经营“以严格的科学管理、先进的工艺技术、精湛的安裝质量、热情周到的服务、赢得社会和顾客的信誉”为宗旨。
+                        </div> <a href="#" class="ibtn more">more </a></div></div></div></section>`,1),q=h({__name:"index",setup(z){const f=[{imageName:"潮州臻顺化工食品",imageUrl:"swiper_1.png"},{imageName:"潮州臻顺化工食品",imageUrl:"swiper_2.png"},{imageName:"潮州臻顺化工食品",imageUrl:"swiper_3.png"}],w=[{imageName:"果汁/果酱锅",imageUrl:"product_1.jpeg"},{imageName:"料仓",imageUrl:"product_2.jpeg"},{imageName:"CIP酸碱桶",imageUrl:"product_3.jpeg"},{imageName:"沙拉设备",imageUrl:"product_4.jpeg"},{imageName:"80M3反应釜",imageUrl:"product_5.jpeg"},{imageName:"205M3换热器",imageUrl:"product_6.jpeg"},{imageName:"回收系统",imageUrl:"product_7.jpeg"},{imageName:"1KL~30KL废液罐",imageUrl:"product_8.jpeg"},{imageName:"2.5KL搅胶机",imageUrl:"product_9.jpeg"},{imageName:"打胶系统",imageUrl:"product_10.jpeg"},{imageName:"冷凝器",imageUrl:"product_11.jpeg"},{imageName:"大料仓",imageUrl:"product_12.jpeg"}];S(()=>window.addEventListener("scroll",N,!0));function N(){window.scrollY>document.querySelector(".site-header").offsetHeight?document.body.classList.add("header-fixed"):document.body.classList.remove("header-fixed")}return(e,K)=>{const m=b,p=P,g=U;return c(),l(d,null,[a("div",k,[t(g,{height:300,modules:["SwiperAutoplay"in e?e.SwiperAutoplay:s(y),"SwiperEffectFade"in e?e.SwiperEffectFade:s(j),"SwiperPagination"in e?e.SwiperPagination:s(_),"SwiperNavigation"in e?e.SwiperNavigation:s(L)],"slides-per-view":1,loop:!0,effect:"fade",navigation:"",pagination:{clickable:!0},autoplay:{delay:6e3,disableOnInteraction:!0},"creative-effect":{prev:{shadow:!1,translate:["-20%",0,-1]},next:{translate:["100%",0,0]}}},{default:r(()=>[(c(),l(d,null,u(f,(i,o)=>t(p,{key:o},{default:r(()=>[t(m,{class:"lazyload",itemprop:"image",src:`/images/${i.imageUrl}`,"data-src":`${i.imageUrl}`,title:`${i.imageName}`,alt:`${i.imageName}`},null,8,["src","data-src","title","alt"])]),_:2},1024)),64))]),_:1},8,["modules"])]),n(),a("main",G,[a("section",x,[a("div",C,[a("div",E,[T,n(),a("div",V,[t(g,{height:300,modules:["SwiperGrid"in e?e.SwiperGrid:s(I),"SwiperPagination"in e?e.SwiperPagination:s(_)],"slides-per-view":4,"slides-per-group":4,"space-between":50,loop:!0,pagination:{clickable:!0},"creative-effect":{prev:{shadow:!1,translate:["-20%",0,-1]},next:{translate:["100%",0,0]}}},{default:r(()=>[(c(),l(d,null,u(w,(i,o)=>t(p,{key:o},{default:r(()=>[t(m,{class:"lazyload",itemprop:"image",src:`/images/common/product/${i.imageUrl}`,"data-src":`${i.imageUrl}`,title:`${i.imageName}`,alt:`${i.imageName}`},null,8,["src","data-src","title","alt"]),n(),a("div",A,[a("div",B,$(i.imageName),1)])]),_:2},1024)),64))]),_:1},8,["modules"])])])])]),n(),F,n(),M])],64)}}});export{q as default};
